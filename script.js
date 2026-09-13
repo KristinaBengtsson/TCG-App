adfe47d472494bab454d2f8bc3ae6b65824174c1
Langemanden <sofus.juul@live.dk>
2026-09-13 23:21:51 +0200

    k

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+    // MENU
     const menuButtons = document.querySelectorAll(".menu-button");
 
     menuButtons.forEach((button) => {
         button.addEventListener("click", function () {
+
             const pageName = button.textContent.trim();
 
             switch (pageName) {
+
                 case "Dashboard":
                     window.location.href = "dashboard.html";
                     break;
@@ -21,10 +25,23 @@ document.addEventListener("DOMContentLoaded", function () {
                 case "Wishlist":
                     window.location.href = "wishlist.html";
                     break;
-
-                default:
-                    console.log("Siden findes ikke endnu.");
             }
+
         });
     });
+
+
+    // PROFIL
+    const profileButton = document.getElementById("profileButton");
+
+    if (profileButton) {
+
+        profileButton.addEventListener("click", function () {
+
+            window.location.href = "profile.html";
+
+        });
+
+    }
+
 });
