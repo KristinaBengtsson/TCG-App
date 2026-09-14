94a0f4d2b8c5304e5ccdcbdea632df9aa3987f52
KristinaBengtsson <cph-kd104@stud.ek.dk>
2026-09-14 09:11:28 +0200

    html collection, dashboard and mastersets update. dashboard.js and masretsets.js added.

---
+++ js/dashboard.js
@@ -0,0 +1,4 @@
+const welcome = document.querySelector("#welcome-text"); //Find id welcome-text og gem det i variabelen welcome
+console.log(welcome.textContent); //udskriv id welcome
+welcome.textContent = "Good evening, Kristina"; //Skift teksten
+welcome.style.color = "#6b5cff"; //Skift farve
